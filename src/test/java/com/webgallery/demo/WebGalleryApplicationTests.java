fb8afe7574bff5e6383b4d61e514b9080a5691a5
Brandon <brandon.uan@gmail.com>
2021-11-24 00:00:10 -0500

    Estructura basica del Backend

---
+++ src/test/java/com/webgallery/demo/WebGalleryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webgallery.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebGalleryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
